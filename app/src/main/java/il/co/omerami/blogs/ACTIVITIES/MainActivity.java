9a83217cd9ad419397e5398c5c9927b21816319b
omerami1 <omeraminov1@gmail.com>
2023-09-09 21:19:17 +0300

    Initial commit

---
+++ app/src/main/java/il/co/omerami/blogs/ACTIVITIES/MainActivity.java
@@ -0,0 +1,16 @@
+package il.co.omerami.blogs.ACTIVITIES;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import il.co.omerami.blogs.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Blogs"
+include(":app")
+include(":MODEL")
+include(":VIEWMODEL")
+include(":REPOSITORY")
+include(":HELPER")
